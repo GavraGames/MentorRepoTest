7631f530152df1057d72ef2cc68ad38899ebb9cd
Lior Hadashian <lior@gavra-games.com>
2023-01-27 10:48:23 +0200

    Modified rotation in NewScript.

--- Repo Project/Assets/NewScript.cs
+++ Repo Project/Assets/NewScript.cs
@@ -9,7 +9,7 @@ public class NewScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.rotation = Quaternion.identity;
     }
 
     // Update is called once per frame
