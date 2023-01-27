c9f34c0e216f965627bd198b03dc6c8de061789a
Lior Hadashian <lior@gavra-games.com>
2023-01-27 10:58:04 +0200

    My angle is the best. f*ck other angles.

--- Repo Project/Assets/NewScript.cs
+++ Repo Project/Assets/NewScript.cs
@@ -9,7 +9,7 @@ public class NewScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.rotation = Quaternion.identity;
+        transform.eulerAngles = new Vector3(1,2,3);
     }
 
     // Update is called once per frame
